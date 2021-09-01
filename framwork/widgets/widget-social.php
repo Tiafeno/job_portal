3a869919ec1937d480f7b5abd3799e2aa16af07f
Tiafeno <tiafenofnel@gmail.com>
2021-09-01 21:47:24 +0300

    Fix widget

--- framwork/widgets/widget-social.php
+++ framwork/widgets/widget-social.php
@@ -17,22 +17,24 @@ class wtsocial_widget extends WP_Widget
     {
         $title = apply_filters('widget_title', $instance['title']);
         $description = $instance['desc'];
+        $custom_logo_id = get_theme_mod('custom_logo');
+        $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
         // before and after widget arguments are defined by themes
         echo $args['before_widget'];
-        echo '<a href="#"><img class="footer-logo" src="" alt=""></a>';
-        if (!empty($title)) echo $args['before_title'] . $title . $args['after_title'];
+        echo sprintf('<a href="%s"><img class="footer-logo" src="%s" alt=""></a>', home_url('/'), esc_url($logo[0]));
+        //if (!empty($title)) echo $args['before_title'] . $title . $args['after_title'];
         // This is where you run the code and display the output
         echo sprintf('<p>%s</p>', $description);
         echo "<div class='f-social-box'>";
         echo " <ul>";
         if (!empty($instance['facebook_link'])) {
-            echo sprintf('<li>%s</li>', $instance['facebook_link']);
+            echo sprintf('<li><a href="%s"><i class="fa fa-facebook facebook-cl"></i></a></li>', $instance['facebook_link']);
         }
         if (!empty($instance['twitter_link'])) {
-            echo sprintf('<li>%s</li>', $instance['twitter_link']);
+            echo sprintf('<li><a href="%s"><i class="fa fa-twitter twitter-cl"></i></a></li>', $instance['twitter_link']);
         }
         if (!empty($instance['instagram_link'])) {
-            echo sprintf('<li>%s</li>', $instance['instagram_link']);
+            echo sprintf('<li><a href="%s"><i class="fa fa-instagram instagram-cl"></i></a></li>', $instance['instagram_link']);
         }
         echo " </ul>";
         echo "</div>";
@@ -41,16 +43,16 @@ class wtsocial_widget extends WP_Widget
 
     public function generate_field($instance, $field, $title = " ", $type = 'input')
     {
-        $field = isset($instance[$field]) ? $instance[$field] : '';
+        $value = isset($instance[$field]) ? $instance[$field] : '';
         $field_id = $this->get_field_id($field);
         $field_name = $this->get_field_name($field);
         ?>
         <label for="<?= $field_id ?>"><?= $title ?></label>
         <?php if ($type === 'input'): ?>
-        <input class="widefat" type="text" id="<?= $field_id ?>" name="<?= $field_name ?>" value="<?= esc_attr($field) ?>">
+        <input class="widefat" type="text" id="<?= $field_id ?>" name="<?= $field_name ?>" value="<?= esc_attr($value) ?>">
     <?php endif; ?>
         <?php if ($type === 'textarea'): ?>
-        <textarea class="widefat" id="<?= $field_id ?>" name="<?= $field_name ?>"><?= esc_attr($field) ?></textarea>
+        <textarea class="widefat" id="<?= $field_id ?>" name="<?= $field_name ?>"><?= esc_attr($value) ?></textarea>
     <?php endif; ?>
         <?php
     }

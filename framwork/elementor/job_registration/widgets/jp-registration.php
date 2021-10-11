63fccdd71f6d8d87fbb2fca16c257dac6bea7453
Tiafeno <tiafenofnel@gmail.com>
2021-10-11 20:15:04 +0300

    not singular

--- framwork/elementor/job_registration/widgets/jp-registration.php
+++ framwork/elementor/job_registration/widgets/jp-registration.php
@@ -131,6 +131,9 @@ add_action('action_jobportal_register', function() {
 add_action('wp_loaded', 'pre_process_registration', 1);
 function pre_process_registration() {
     //if (!is_singular()) return;
+    $reg = \jpHelpers::getValue('reg', false);
+    var_dump($reg);
+    wp_die();
     if (\jpHelpers::getValue('reg', false)) {
         // Enregistrer les informations utilisateur
         do_action('action_jobportal_register');

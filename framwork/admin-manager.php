446c6557b5ca71b4b9cc4c0d1e2d1b8e912d4d3d
Tiafeno <tiafenofnel@gmail.com>
2021-10-11 19:40:29 +0300

    Minor fix

--- framwork/admin-manager.php
+++ framwork/admin-manager.php
@@ -4,7 +4,6 @@ final class AdminManager {
     public function __construct() {
         add_filter( 'manage_users_columns', [&$this, 'user_head_table'] );
         add_filter( 'manage_users_custom_column', [&$this, 'manage_user_table'], 10, 3 );
-        add_filter( 'user_row_actions', 'activation_user_action', 10, 2);
         add_action('admin_init', [&$this, 'init']);
     }
 
@@ -19,19 +18,10 @@ final class AdminManager {
                 $user = WP_User::get_data_by('ID', $user_id);
                 $is_active = get_metadata('user', $user->ID, 'is_active', true);
                 $action_name = $is_active ? "deactivated" : "activated";
-                return "<a class='activation btn' href='" . admin_url( "users.php?&action=user_activation&amp;user=$user->ID&amp;ref=$action_name") . "'>" . ucfirst($action_name) . "</a>";
+                return "<a class='activation button' href='" . admin_url( "users.php?action=user_activation&amp;user=$user->ID&amp;ref=$action_name") . "'>" . ucfirst($action_name) . "</a>";
             default:
         }
         return $val;
-    }
-
-    public function activation_user_action($actions, WP_User $user) {
-        $user_role = reset($user->roles);
-        if (!in_array($user_role, ['candidate', 'company'])) return $actions;
-        $is_active = get_metadata('user', $user->ID, 'is_active', true);
-        $action_name = $is_active ? "deactivated" : "activated";
-        $actions['activation'] = "<a class='activation' href='" . admin_url( "users.php?&action=user_activation&amp;user=$user->ID&amp;ref=$action_name") . "'>" . ucfirst($action_name) . "</a>";
-        return $actions;
     }
 
     public function init() {
@@ -43,7 +33,8 @@ final class AdminManager {
             $ref = jpHelpers::getValue('ref');
             $value = $ref === 'deactivated' ? 0 : 1;
             $user_id = (int) jpHelpers::getValue('user');
-            $reponse = update_metadata('user', $user_id, 'is_active', $value);
+            // Mettre a jours la valeur
+            update_metadata('user', $user_id, 'is_active', $value);
         }
     }
 }
